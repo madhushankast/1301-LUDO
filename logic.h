69dbe5fef1cec7b7873239c026a0a148cd43a113
madhushankast <stmadhushanka067@gmail.com>
2024-08-15 17:56:34 +0530

    with a.out

--- logic.h
+++ logic.h
@@ -4,18 +4,29 @@
 #include "type.h"
  
 int LUDO();
+
 void gamerun(int, int);
+
 void move(int);
+
 char checkcometox(player*,int);
+
 void standmove(player* player,int x);
 
 int randompiece();
+
 int checkpos(int);
+
 void printbegin();
+
 int roll();
+
 int firstroll(int i);
+
 void printvalues(int *ptr);
+
 int sort(int *arr);
+
 int begin();
 
 #endif
